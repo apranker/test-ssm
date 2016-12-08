659e24e7e0d94af65a695a342c1a845b33a7db30
dayong.gao <dayong.gao@qunar.com>
2016-12-08 11:41:34 +0800

    Dubbo demo 问题修改

--- src/main/java/com/qunar/dubbo/MsgInfo.java
+++ src/main/java/com/qunar/dubbo/MsgInfo.java
@@ -6,15 +6,16 @@ import java.util.List;
  * Created by dayong.gao on 2016/12/7.
  */
 public class MsgInfo implements Serializable {
-    private static final long serialVersionUID = -2814022769568306965L;
 
+    private static final long serialVersionUID = 475340319429306958L;
     private int id;
     private String name;
-    private List<String>msgs;
+    private List<String> msgs;
     public int getId() {
         return id;
     }
     public void setId(int id) {
+
         this.id= id;
     }
     public String getName() {
